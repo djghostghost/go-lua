d26ee8c48592fd48ac6ca151befe9331bb97c33f
djghostghost <ligx746@gmail.com>
2022-07-04 09:19:37 +0900

    add lexer

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"github.com/djghostghost/go-lua/compiler/lexer"
 	"io/ioutil"
 	"os"
@@ -12,7 +13,7 @@ func main() {
 	if len(os.Args) > 1 {
 		luacFile = os.Args[1]
 	} else {
-		luacFile = "./lua/lexer.out"
+		luacFile = "./lua/hello.lua"
 	}
 
 	data, err := ioutil.ReadFile(luacFile)
@@ -20,13 +21,38 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
-
+	testLexer(string(data), luacFile)
 }
 
 func testLexer(chunk, chunkName string) {
 	l := lexer.NewLexer(chunk, chunkName)
 
-	for
-
+	for {
+		line, kind, token := l.NextToken()
+		fmt.Printf("[%2d] [%-10s] %s\n", line, kindToCategory(kind), token)
+		if kind == lexer.TokenEoF {
+			break
+		}
+	}
 }
 
+func kindToCategory(kind int) string {
+	switch {
+	case kind < lexer.TokenSepSemi:
+		return "other"
+	case kind <= lexer.TokenSepRCurly:
+		return "separator"
+	case kind <= lexer.TokenOpNot:
+		return "operator"
+	case kind <= lexer.TokenKwWhile:
+		return "keyword"
+	case kind == lexer.TokenIdentifier:
+		return "identifier"
+	case kind == lexer.TokenNumber:
+		return "number"
+	case kind == lexer.TokenString:
+		return "string"
+	default:
+		return "other"
+	}
+}
